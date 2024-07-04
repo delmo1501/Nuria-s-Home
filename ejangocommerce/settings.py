7621aedb19124856d3ffac50fccb2cf5bc40fe30
bruno <bruno.delmoro@adeccogroup.com>
2024-07-04 17:26:59 +0200

    zvzcxv

--- ejangocommerce/settings.py
+++ ejangocommerce/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-30z9k3*+fca^+&9w@+myn!dnmoc4t60rx5#tp@1x^hrt-_01p7')
@@ -59,15 +60,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ejangocommerce.wsgi.application'
 
-# Configuración de la base de datos, utiliza una base de datos robusta para producción
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('postgres'),
+        'USER': os.environ.get('postgres.dfsahcevjzgustgyckgt'),
+        'PASSWORD': os.environ.get('14631463Delmo-'),
+        'HOST': os.environ.get('aws-0-eu-central-1.pooler.supabase.com'),
+        'PORT': os.environ.get('6543'),
     }
 }
 
@@ -101,3 +101,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print("DB_NAME:", os.environ.get('DB_NAME'))
+print("DB_USER:", os.environ.get('DB_USER'))
+print("DB_PASSWORD:", os.environ.get('DB_PASSWORD'))
+print("DB_HOST:", os.environ.get('DB_HOST'))
+print("DB_PORT:", os.environ.get('DB_PORT'))
